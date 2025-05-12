1492af8f90a19f3e7bf68984c51000cb46767209
Diego Denzer <diego.denzer@e-governe.com>
2025-05-12 09:04:52 -0300

    teste 5

--- js/translations.js
+++ js/translations.js
@@ -1,10 +1,15 @@
 import translations from './translations.json';
 
-function setLanguage(lang) {
+export function setLanguage(lang) {
+    console.log(`Changing language to: ${lang}`);
     const elements = document.querySelectorAll("[data-translate]");
     elements.forEach(el => {
         const key = el.getAttribute("data-translate");
+        if (translations[lang] && translations[lang][key]) {
         el.textContent = translations[lang][key];
+    } else {
+        console.warn(`Translation missing for key: ${key} in language: ${lang}`);
+    }
     });
 }
 
